e15a3d3b33a5b0cae57a7bf2ee34f5287410584b
Eugenio Mosca <eugenio.mosca@gmail.com>
2026-03-25 15:27:02 +0100

    Update API_URL to new endpoint

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
-const API_URL = "https://script.google.com/macros/s/AKfycbz0mEy5B_Av_i4SmmNOiTkeeURpA7s_9bU4Ghcv8kBnP0f3MhK691IKZaGKV2iUdlzN5Q/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxIWBXLkAEfkibT0eAFM30Ys59nN9u1byJkZDA-eiQ8peQH2tJgkcqEyH7lljgErJVeFw/exec";
 
 // timeout massimo della chiamata API in millisecondi
 const API_TIMEOUT_MS = 15000;
