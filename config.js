82b38b359d4a99bf76aa52cca92a3474eacf7fbf
Eugenio Mosca <eugenio.mosca@gmail.com>
2026-03-25 14:44:22 +0100

    Update API_URL to point to the new endpoint

---
+++ config.js
@@ -0,0 +1,3 @@
+// config.js
+// Qui metti il link alla tua API
+const API_URL = "https://script.google.com/macros/s/AKfycbwZfl2AzksQTnXMpi2tqZUr9ZrHUHAb9XMqIl2EA6iSVRYqVLe2Y-Cax7RCjHTvCzbOCQ/exec";
